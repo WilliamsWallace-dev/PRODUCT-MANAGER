77510377c81a537e5eea28b36504268950ce9018
Williams Wallace <wwallace.dev@gmail.com>
2023-11-27 20:55:40 -0300

    vercel

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
-// module.exports = {
-//       dialect: 'postgres',
-//       host: 'localhost',
-//       port: '5432',
-//       database: 'bookstore_development',
-//       username: "postgres",
-//       password: "wallace357",
-//   }
+module.exports = {
+      dialect: 'postgres',
+      host: 'localhost',
+      port: '5432',
+      database: 'bookstore_development',
+      username: "postgres",
+      password: "wallace357",
+  }
 
-import pg from 'pg';
+// import pg from 'pg';
 
-const { Pool } = pg;
+// const { Pool } = pg;
 
-export const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-})
+// export const pool = new Pool({
+//   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+// })
 
-pool.connect((err)=>{
-    if(err) throw err
-    console.log("Connect to PostgreSQL successfully")
-})
+// pool.connect((err)=>{
+//     if(err) throw err
+//     console.log("Connect to PostgreSQL successfully")
+// })
--- config/sequelize.ts
+++ config/sequelize.ts
@@ -1,9 +1,8 @@
 import {Sequelize} from "sequelize"
 const sequelize = require("../config/config.js")
 
-
-export const database = new Sequelize({
-    ...sequelize,
+// @ts-ignore
+export const database = new Sequelize(process.env.POSTGRES_URL || "postgres://default:5CWGoBFAj8RD@ep-super-water-98411280-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb" + "?sslmode=require",{
     define : {
         underscored : true
     }
