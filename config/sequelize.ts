4d811e6f5000aa586f69f07c73a4e13646a5eb0d
Williams Wallace <wwallace.dev@gmail.com>
2023-11-27 20:57:00 -0300

    vercel

--- config/sequelize.ts
+++ config/sequelize.ts
@@ -2,7 +2,7 @@ import {Sequelize} from "sequelize"
 const sequelize = require("../config/config.js")
 
 // @ts-ignore
-export const database = new Sequelize(process.env.POSTGRES_URL || "postgres://default:5CWGoBFAj8RD@ep-super-water-98411280-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb" + "?sslmode=require",{
+export const database = new Sequelize(process.env.POSTGRES_URL+"?sslmode=require" || "postgres://default:5CWGoBFAj8RD@ep-super-water-98411280-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb" + "?sslmode=require",{
     define : {
         underscored : true
     }
